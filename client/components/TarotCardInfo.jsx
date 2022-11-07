959f9fcb33d50ef727c1d9f762639d4a1665ab9e
root <root@PhilYoo.localdomain>
2022-11-07 12:47:19 -0800

    Add X button for modal closing

--- client/components/TarotCardInfo.jsx
+++ client/components/TarotCardInfo.jsx
@@ -20,12 +20,13 @@ const TarotCardInfo = ({deck, cardDirection}) => {
     <>
       <input type="checkbox" id="my-modal-6" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
+        <div className="modal-box relative">
+          <label htmlFor="my-modal-6" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
           <h3 className="font-bold text-lg">{deck.name} | {cardDirection}</h3>
           <p className="py-4">{deck.description}</p>
           {moreInfo}
           <div className="modal-action">
-            <label htmlFor="my-modal-6" className="btn">Yay!</label>
+            <label htmlFor="my-modal-6" className="btn">Do something</label>
           </div>
         </div>
       </div>
