23c922afda64ffd720cf83c1741bf5c5db734050
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-11 17:56:10 -0800

    Add initial working steps component

--- client/components/ReadingType.jsx
+++ client/components/ReadingType.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const ReadingType = ({type, description}) => {
+const ReadingType = ({type, description, next}) => {
   return (
-    <div className="card card-normal min-w-0 w-96 bg-base-100 shadow-xl">
+    <div className="card card-normal w-96 h-[600px] bg-base-100 shadow-xl">
       <figure className="px-10 pt-10 basis-1/2">
         <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl object-cover h-full" />
       </figure>
@@ -10,7 +10,7 @@ const ReadingType = ({type, description}) => {
         <h2 className="card-title">{type}</h2>
         <p>{description}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">start</button>
+          <button className="btn btn-primary" onClick={next}>start</button>
         </div>
       </div>
     </div>
---
+++ client/components/Steps.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+
+const Steps = ({stepOne, stepTwo, stepThree, currentStep}) => {
+
+  return (
+    <ul className="steps steps-vertical w-[90vw] text-neutral text-2xl font-bold lg:steps-horizontal">
+      <li className={`step ${currentStep >= 0 && 'step-primary'}`}>{stepOne}</li>
+      <li className={`step ${currentStep >= 1 && 'step-primary'}`}>{stepTwo}</li>
+      <li className={`step ${currentStep >= 2  && 'step-primary'}`}>{stepThree}</li>
+    </ul>
+  )
+}
+
+export default Steps;
