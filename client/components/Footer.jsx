cde1e8e0a1cac7c8e99745a97ad5198657ff742f
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-19 02:24:28 -0800

    Update navbar and footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { SiGithub } from 'react-icons/si';
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-10 bg-primary text-primary-content">
-      <div>
-        <p className="font-bold">
-          Created by josemourinho333
-        </p> 
-        <p>Copyright © 2022 - All right reserved</p>
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+      <div className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About</a> 
+        <a className="link link-hover">Learn</a> 
+        <a className="link link-hover">Contact</a> 
       </div> 
+      <div>
+        <div className="grid grid-flow-col gap-4">
+          <a href="https://github.com/josemourinho333/tarot-me" target="_blank"><SiGithub className="w-8 h-8"/></a>
+        </div>
+      </div> 
+      <div>
+        <p>Created by josemourinho333</p>
+      </div>
     </footer>
   )
 }
