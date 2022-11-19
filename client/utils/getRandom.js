bd362b914cfec6096accf4cf43ba1fda6f284a0b
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-18 16:20:27 -0800

    Update getRandom to also ranomdly select card position

--- client/utils/getRandom.js
+++ client/utils/getRandom.js
@@ -1,12 +1,33 @@
-//get random card when given target and array of cards
+//get random card and their position when given target and array of cards
 const getRandom = (target, cards) => {
   // shuffles
   const shuffledCards = [...cards]
   shuffledCards.sort(() => {
     return Math.random() - 0.5;
   });
-  
-  return shuffledCards.slice(0, target);
+
+  const pickedCards = shuffledCards.slice(0, 3);
+
+  // loop over picked cards and randomly get their positions (upright or reversed)
+  const finalPicked = [];
+  pickedCards.forEach((card) => {
+    let position;
+    const positionIndicator = Math.round(Math.random());
+    if (!positionIndicator) {
+      position = 'reversed';
+    } else {
+      position = 'upright';
+    };
+     
+    const buildCard = {
+      card,
+      position
+    };
+
+    finalPicked.push(buildCard);
+  });
+
+  return finalPicked;
 };
 
 export default getRandom;
