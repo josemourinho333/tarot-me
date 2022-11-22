bb67508b383501fca6be9f040262dcba375c8d41
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-21 23:27:08 -0800

    move mobile drawer button to bottom

--- client/pages/learn.jsx
+++ client/pages/learn.jsx
@@ -85,10 +85,11 @@ const Learn = () => {
     <div className="drawer drawer-mobile">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
+        
+        <LearningMain selected={selected} cards={data} selectHandler={selectHandler} />
         <label htmlFor="my-drawer-2" className="btn btn-ghost drawer-button lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
-          <LearningMain selected={selected} cards={data} selectHandler={selectHandler} />
       </div> 
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
