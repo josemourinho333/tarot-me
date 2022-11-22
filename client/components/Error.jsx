1dd73db912014bce8ff13badc6fdaa79994cc232
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-22 11:29:17 -0800

    Add error comp and add it to learn page

---
+++ client/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className={`h-screen w-screen flex flex-col justify-center items-center text-xl text-neutral-content text-center italic font-light`}>
+       Oops. Something broke. Try refreshing of if it keeps happening contact me on github.
+    </div>
+  )
+}
+
+export default Error;
