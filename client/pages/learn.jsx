eae89b3d2069ea999cf01695cf49484faebf039c
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-21 22:08:50 -0800

    Update mobile menu button icon

--- client/pages/learn.jsx
+++ client/pages/learn.jsx
@@ -53,9 +53,9 @@ const Learn = () => {
   return (
     <div className="drawer drawer-mobile">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-start justify-center">
+      <div className="drawer-content flex flex-col">
         <label htmlFor="my-drawer-2" className="btn btn-ghost drawer-button lg:hidden">
-          <HiMenuAlt1 className="text-neutral-content text-4xl"/>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
         <LearningMain selected={selected} cards={data} selectHandler={selectHandler} />
         {/* <Learn selected={selected} cards={data} selectHandler={selectHandler}/> */}
