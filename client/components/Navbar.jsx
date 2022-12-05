0410ae8313a0ebf22eab2e5c4cf34ee615a22fb4
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-12-05 07:56:24 -0800

    Update navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="navbar fixed top-0 left-0 right-0 bg-base-100/25 backdrop-blur-sm text-base-content px-4 z-50">
+    <div className="navbar fixed top-0 left-0 right-0 bg-base-100/70 backdrop-blur-lg text-base-content px-0 lg:px-4 z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-2 p-2 shadow bg-base-100 text-base-content rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-2 p-2 shadow bg-neutral text-base-content w-screen divide-y divide-solid divide-neutral-content">
             {/* <li tabIndex={0}>
               <a className="justify-between">
                 Reading
@@ -19,11 +19,12 @@ const Navbar = () => {
                 <li><a>Astrology & Horoscope</a></li>
               </ul>
             </li> */}
-            <li><a href="/learn">Learn</a></li>
+            <li ><a href="/reading" className="text-2xl font-light py-5">Reading</a></li>
+            <li ><a href="/learn" className="text-2xl font-light py-5">Learn</a></li>
             {/* <li><a>Shop</a></li> */}
           </ul>
         </div>
-        <a href="/" className="btn btn-ghost normal-case text-2xl">Tarot & I</a>
+        <a href="/" className="btn btn-ghost normal-case text-3xl font-black">Tarot.Me</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
@@ -37,12 +38,13 @@ const Navbar = () => {
               <li><a>Astrology & Horoscope</a></li>
             </ul>
           </li> */}
-          <li><a href="/learn">Learn</a></li>
+          {/* <li><a href="/learn">Learn</a></li> */}
           {/* <li><a>Shop</a></li> */}
         </ul>
       </div>
-      <div className="navbar-end">
-        <a href="/reading" className="btn bg-primary text-primary-content">Get a reading</a>
+      <div className="navbar-end px-[1rem] gap-x-2 lg:px-0 ">
+        <a href="/reading" className="btn btn-sm btn-outline">Get a reading</a>
+        <a href="/learn" className="btn btn-sm btn-outline hidden lg:flex">Learn</a>
       </div>
     </div>
   )
